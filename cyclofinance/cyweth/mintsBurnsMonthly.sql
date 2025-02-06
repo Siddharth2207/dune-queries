bc0a57b436fa9314c992626b555dadc435724a81
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-02-06 16:46:23 +0530

    cyclo query

--- cyclofinance/cyweth/mintsBurnsMonthly.sql
+++ cyclofinance/cyweth/mintsBurnsMonthly.sql
@@ -7,7 +7,7 @@ SELECT
   SUM(ROUND(value / 1e18, 10)) AS total_amount,
   MIN(evt_block_time) AS block_time /* Earliest block time in the month for reference */
 FROM 
-  cyclofinance_flare.ERC20PriceOracleReceiptVault_evt_Transfer
+  cyclofinance_flare.cyclovault_evt_transfer
 WHERE
   contract_address = 0x19831cfb53a0dbead9866c43557c1d48dff76567 /* Bytearray literal */
   AND (
